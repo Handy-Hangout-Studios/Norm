69d0905e5d5c48562dd3f6ce91aee9678c1d68e7
ProfDoof <johnswork15@gmail.com>
2020-12-01 20:58:59 -0500

    Initial Commit

---
+++ Harold/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace Harold
+{
+    public class Program
+    {
+        public static void Main(string[] args) =>
+            CreateHostBuilder(args)
+                .Build()
+                .Run();
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args);
+
+        public static Action<IConfigurationBuilder> ConfigureHostConfiguration(string[] args) =>
+            configuration => configuration
+                .AddJsonFile("config.json", optional: false)
+                .AddCommandLine(args);
+
+        public static void ConfigureHangfireServices(HostBuilderContext context, IServiceCollection services)
+        {
+
+        }
+    }
+}
